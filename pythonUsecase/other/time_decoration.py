f9c22b7fae51a5ad979a8e3084dafc909095b528
CSnotme <924432875@qq.com>
2021-06-21 19:11:11 +0800

    时间装饰器

---
+++ pythonUsecase/other/time_decoration.py
@@ -0,0 +1,38 @@
+"""
+@File        : time_decoration.py
+@Time        : 2021/6/21 19:02
+@Author      : caoyf
+
+@Description : 时间装饰器
+"""
+
+import time
+
+
+def print_record_time(func):
+    """
+    记录函数执行时间并打印
+    :param func:
+    :return:
+    """
+    def inner(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        use_time = time.time() - start_time
+        print(use_time)
+        return result
+    return inner
+
+
+def return_record_time(func):
+    """
+    记录函数执行时间并返回
+    :param func:
+    :return:
+    """
+    def inner(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        use_time = time.time() - start_time
+        return result, use_time
+    return inner
